e47f803d3d4b3bbd77e97689a6ef65ae3bf433ea
Alex <alexandertaylor315@gmail.com>
2022-10-19 00:55:16 -0400

    updated app urls and views files

--- products/urls.py
+++ products/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('', views.product_list)]
+urlpatterns = [
+    path('', views.product_list),
+]
--- products/views.py
+++ products/views.py
@@ -1,10 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from . serializer import ProductSerializer
+from .models import Product
+from products import serializer
 
 # Create your views here.
 
 @api_view('GET')
 def product_list(request):
     
+    products = Product.objects.all()
     
-    return Response('ok')
+    serializer = ProductSerializer(products, many=True)
+    
+    
+    return Response(serializer.data)
