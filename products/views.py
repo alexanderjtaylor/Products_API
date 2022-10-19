cac2f9e6917db57dd37232d4e204d4254e6f0052
Alex <alexandertaylor315@gmail.com>
2022-10-19 01:46:26 -0400

    registered product model with admin site & completed POST functionality for Product & Updated REST framework settings

--- products/views.py
+++ products/views.py
@@ -1,17 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from . serializer import ProductSerializer
 from .models import Product
 from products import serializer
 
 # Create your views here.
 
-@api_view('GET')
+@api_view(['GET', 'POST'])
 def product_list(request):
-    
-    products = Product.objects.all()
-    
-    serializer = ProductSerializer(products, many=True)
-    
-    
-    return Response(serializer.data)
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
